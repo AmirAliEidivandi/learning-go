64380b101d1b552eeb9e1c88a8ffd2f3d78cf8bf
AmirAliEidivandi <amirah648887@gmail.com>
2023-03-09 23:27:29 +0330

    learning-go lecture-1

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const secondsInHour = 3600
+
+func main() {
+	fmt.Println(secondsInHour)
+	fmt.Println("hello")
+	distance := 60.8
+	fmt.Printf("The distance in miles is %f \n", distance*0.621)
+}
---
+++ vars/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int age = 10; // c++ way
+	var age int = 30
+	fmt.Println("My Age:", age)
+
+	var name = "amirali"
+	_ = name
+	// fmt.Println("My name is:", name)
+
+	s := "Learning go"
+	fmt.Println(s)
+}
